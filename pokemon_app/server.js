0373f303007054480570ea97f83277f0754be16d
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-06 21:15:53 -0500

    Show view shows req.params.id

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -10,11 +10,15 @@ app.engine('jsx', require('express-react-views').createEngine());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
-})
+});
 
 app.get('/pokemon', (req, res) => {
     res.render('Index', { pokemon: pokemon });
 });
+
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
