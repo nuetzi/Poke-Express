ce9359a4a364c9b857aa2307b94ced6877bc5214
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-06 21:01:49 -0500

    Index.jsx view rendered at pokemon route

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -3,14 +3,18 @@ const app = express();
 const port = 3000;
 const pokemon = require('./models/pokemon.js');
 
+// Set up view engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon);
-})
+    res.render('Index', { pokemon: pokemon });
+});
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
---
+++ pokemon_app/views/Index.jsx
@@ -0,0 +1,31 @@
+const React = require('react');
+
+
+class Index extends React.Component {
+    render() {
+        const { pokemon } = this.props;
+        const myStyle = {
+        color: '#ffffff',
+        backgroundColor: '#000000',
+         };     
+        return (
+          <div style={myStyle}>
+              <h1>See All The Pokemon!</h1>
+              <ul>
+                    {pokemon.map((pokemon, i) => {
+                        return (
+                            <li>
+                            <a href={`/pokemon/${i}`}>
+                            {pokemon.name}
+                            </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+          )  
+     }
+}
+
+
+module.exports = Index;
