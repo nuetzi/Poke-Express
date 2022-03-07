a7feb7a2e29c785f0405e838e9c1ef9d05324d33
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-06 21:45:50 -0500

    Created show views of each Pokemon - Assignment Complete

--- pokemon_app/views/Show.jsx
+++ pokemon_app/views/Show.jsx
@@ -14,7 +14,8 @@ class Show extends React.Component {
             <div style={myStyle}>
                 <h1>Gotta Catch 'Em All!</h1>
                 <h2> {pokemon.name} </h2>
-                <img src={pokemon.img}></img>
+                <img src={pokemon.img + '.jpg'}></img>
+                <br></br>
                 <br></br>
                 <a href='/pokemon'>Back</a>
             </div>
