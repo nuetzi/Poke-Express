84214d79593b710a0bee2a77aeff35955d8caab2
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 16:19:12 -0500

    Fixed mistake in Edit.jsx

--- views/Edit.jsx
+++ views/Edit.jsx
@@ -6,11 +6,10 @@ class Edit extends React.Component{
     return (
       <DefaultLayout title="Edit Page">
         <link rel="stylesheet" href="/css/app.css"/>      
-        {/* The Layout takes in a prop called Title and we pass the Edit Page to it  note: comments can't go first or last in  jsx return*/}
-        {/* form is not complete we will do that below*/}
+        {/* The Layout takes in a prop called Title and we pass the Edit Page to it */}
         <form action={`/pokemon/${this.props.pokemon._id}?_method=PUT`} method="POST">
         Name: <input type="text" name="name" defaultValue={this.props.pokemon.name}/><br/>
-        Image: <input type="text" name="color"  defaultValue={this.props.pokemon.img}/><br/>
+        Image: <input type="text" name="img"  defaultValue={this.props.pokemon.img}/><br/>
         <br/>
         <br/>
         <input type="submit" value="Submit Changes"/>
