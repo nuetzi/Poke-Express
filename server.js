1670112aec29767abd63beb304ae0d1d572cfb4a
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 16:04:38 -0500

    Added charmeleon to the seed data

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/pokemon/seed', (req, res) => {
         {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
         {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
         {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+        {name: "charmeleon", img: "http://img.pokemondb.net/artwork/charmeleon"},
         {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
         {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
         {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"},
