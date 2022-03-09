8988220da3e981a1c6d27191efd07b258f66737e
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-03-09 16:26:40 -0500

    Create a button for EDIT, instead of a link

--- views/Show.jsx
+++ views/Show.jsx
@@ -15,6 +15,10 @@ class Show extends React.Component {
                 <img src={pokemon.img + '.jpg'}></img>
                 <br/>
                 <br/>
+                <form action={`/pokemon/${pokemon._id}/edit`}>
+                <input type="submit" value="EDIT"/>
+                </form>
+                <br/>
                 <a href={`/pokemon/${pokemon.id}/edit`}>Edit This Pokemon</a>
                 <form action={`/pokemon/${pokemon._id}?_method=DELETE`} method="POST">
                 <input type="submit" value="DELETE"/>
